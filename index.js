5feacae8f8e7a2dd3e41724860d59d747f44e516
gloria oduor <gloriaoduor@gmail.com>
2022-06-12 22:10:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string1) {
+    return drivers.filter((match) => match.toLowerCase() === string1.toLowerCase());
+}
+  
+function fuzzyMatch(drivers, string2) {
+    return drivers.filter((elementMatch) => elementMatch.toLowerCase().indexOf(string2.toLowerCase()) === 0);
+}
+
+function matchName(drivers, string3) {
+    return drivers.filter((record) => record.name === string3);
+}
